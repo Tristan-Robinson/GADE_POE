3356941af47d72f93c5f228c1824e6b800036207
Tristan-Robinson <ST10439581@vgconnect.edu.za>
2026-03-18 19:00:53 +0200

    Hint system

---
+++ Assets/Scripts/HintTrigger.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class HintTrigger : MonoBehaviour
+{
+    [TextArea]
+    public string hintMessage;
+
+    public HintManager hintSystem;
+
+    public bool showOnlyOnce = true;
+    public bool hasTriggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hasTriggered && showOnlyOnce) 
+            return;
+        if (other.CompareTag("Player"))
+        {
+            hintSystem.ShowHint(hintMessage);
+            hasTriggered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            hintSystem.HideHint();
+        }
+    }
+}
