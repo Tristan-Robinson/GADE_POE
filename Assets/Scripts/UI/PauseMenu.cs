2e93f8b87bca19e57c245eba37a8d3cee4788206
Tristan-Robinson <ST10439581@vgconnect.edu.za>
2026-06-05 12:57:45 +0200

    Pause menu not working

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,16 +1,76 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject pauseMenuUI;
+    private bool isPaused = false;
+
+    public GameObject pausePanel;
+    public GameObject settingsPanel;
+
+    private void Start()
     {
-        
+        pauseMenuUI.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPause(InputAction.CallbackContext context)
     {
-        
+        if (!context.performed)
+        {
+            return;
+        }
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OpenSettings()
+    {
+        pausePanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        pausePanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void QuitToMenu()
+    {
+       Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
